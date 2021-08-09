3266fa83d392bc8712b626de8528096c13dae03d
lkl191 <shigotoyou1985@gmail.com>
2021-08-09 13:03:55 +0900

    adsense

---
+++ components/meta.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const Meta = () => {
+  return (
+      <Head>
+        <title>Graph App</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <script data-ad-client="ca-pub-2772775021248349" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+      </Head>
+  );
+};
+
+export default Meta;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,14 @@ import { ApolloProvider } from "@apollo/client";
 import client from "../components/graphql/client";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import Meta from "../components/meta";
 
 //_app.jsは二回レンダリングされる
 const MyApp = ({ Component, pageProps }) => {
   return (
     <>
       <ApolloProvider client={client}>
+        <Meta />
         <Header />
         <Component {...pageProps} />
         <Footer />
