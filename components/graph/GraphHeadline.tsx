d4ffd61feeeb75e4feacdfa96d0416a558561606
ushinohama966 <ushinohama966@gmail.com>
2022-06-12 10:55:35 +0900

    feat: implement card UI in allGraphs

--- components/graph/GraphHeadline.tsx
+++ components/graph/GraphHeadline.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { Graph, GraphKind } from "../../types/types";
 
 export const graphImage = (graphKind: GraphKind) => {
-  let imagePath;
+  let imagePath: string;
   switch (graphKind) {
     case "BAR":
       imagePath = "/Graphs/bar.png";
@@ -26,38 +26,17 @@ const GraphHeadline = ({ graphs }: { graphs: Graph[] }) => {
   return (
     <div className="graph-card-wrapper">
       {graphs.map((e) => (
-        <div key={e.id} className="graph-card">
-          <Link href={`/graph/singleGraph?id=${e.id}`}>
-            <p>{e.title}</p>
+        <Link href={`/graph/singleGraph?id=${e.id}`} className="">
+          <div key={e.id} className="graph-card">
+            <h3>{e.title}</h3>
+            <p className="graph-description">{e.description}</p>
             <Image
               src={graphImage(e.graphKind)}
               width="75"
               height="75"
             />
-          </Link>
-          {/* <Link href={`/graph/singleGraph?id=${e.id}`}>
-            <a>
-              <h3 className="graph-index">
-                <div className="flex">
-                  <div className="graph_image">
-                    <Image
-                      src={graphImage(e.graphKind)}
-                      width="75"
-                      height="75"
-                    />
-                  </div>
-                  <div>
-                    <span className="graph_title">{e.title}</span>
-                    {e.description && (
-                      <p className="description_allGraph">{e.description}</p>
-                    )}
-                  </div>
-                </div>
-              </h3>
-            </a>
-          </Link>
-          <br /> */}
-        </div>
+          </div>
+        </Link>
       ))}
     </div>
   );
